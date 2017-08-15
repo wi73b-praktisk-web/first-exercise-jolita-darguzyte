680e2e43e1b8c7101f352ed2ef5115d1a6924200
jolitadarguzyte <4364891@rts-365.dk>
2017-08-15 14:15:36 +0200

    Ovelse

---
+++ ovelse2.js
@@ -0,0 +1,9 @@
+var moms = 1.25;
+var pris_uden_moms = 400;
+var pris_med_moms =  pris_uden_moms *  moms;
+var pris_med_moms_02 = pris_med_moms / moms;
+
+
+
+console.log(pris_med_moms);
+console.log(pris_med_moms_02);
---
+++ ovelse3.js
@@ -0,0 +1,7 @@
+var forste = 10;
+var anden = 20;
+var temp = forste;
+forste = anden ;
+anden = temp;
+
+console.log(forste +" "+ anden);
