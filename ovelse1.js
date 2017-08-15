0ec1a6213d3dce14d597e2a6bedce0e1449c9d69
jolitadarguzyte <4364891@rts-365.dk>
2017-08-15 11:15:03 +0200

    test

---
+++ ovelse1.js
@@ -0,0 +1,8 @@
+
+var navn = "Jolita";
+var alder = 38;
+var tekst = "Mit navn er " + navn + " og jeg er " + alder + " år gammel. Navnet og alderen i teksten skal komme fra dine variabler";
+
+
+
+console.log(tekst); 
